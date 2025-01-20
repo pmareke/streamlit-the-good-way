727acb6e048368f39a48fef270421f78b104d2b6
Pedro López Mareque <pedro.lopez.mareque@gmail.com>
2025-01-20 10:50:45 +0100

    feat: first implementation of the game

--- main.py
+++ main.py
@@ -1,3 +1,32 @@
 import streamlit as st
 
-st.title("Hello World")
+st.title("Fun with Flags")
+
+button = st.button("Play again!")
+
+if button:
+    st.rerun()
+
+st.write("Which country does this flag belong to?")
+
+
+def _get_random_flag() -> tuple[str, dict]:
+    countries = {
+        "Spain": True,
+        "France": False,
+        "Germany": False,
+    }
+    return ("https://flagcdn.com/es.svg", countries)
+
+
+flag, countries = _get_random_flag()
+
+st.image(flag)
+
+for idx, (name, is_ok) in enumerate(countries.items()):
+    button = st.button(name, key=f"button_{name}")
+    if button:
+        if is_ok:
+            st.write("Correct!")
+        else:
+            st.write("Incorrect, try again!")
