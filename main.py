293fb099d5c857aa60c815cb469c8e8e5d4e8e40
Pedro López Mareque <pedro.lopez.mareque@gmail.com>
2025-01-20 15:29:00 +0100

    feat: add a new tab to guess the capital of a country

--- main.py
+++ main.py
@@ -4,50 +4,102 @@ import streamlit as st
 
 st.header("Fun with Flags")
 
-st.subheader("Which country does this flag belong to?")
+tab_1, tab_2 = st.tabs(["Guess the Flag", "Guess the Capital"])
 
-if not st.session_state.get("selected_countries"):
-    response = requests.get("https://restcountries.com/v3.1/all?fields=name,flags")
-    json_response = response.json()
-    length = len(json_response)
-    random_idx = np.random.randint(0, length, 3)
-    selected_countries = {}
-    for idx in random_idx:
-        country = json_response[idx]
-        name = country["name"]["common"]
-        flag = country["flags"]["svg"]
-        selected_countries[name] = flag
-    st.session_state.selected_countries = selected_countries
+with tab_1:
+    st.subheader("Which country does this flag belong to?")
 
-    random_country = np.random.choice(list(selected_countries.keys()))
-    st.session_state.random_country = random_country
-    countries = {}
-    for idx, (name, flag) in enumerate(selected_countries.items()):
-        if name == random_country:
-            countries[name] = True
-        else:
-            countries[name] = False
-    st.session_state.countries = countries
+    if not st.session_state.get("selected_countries"):
+        response = requests.get("https://restcountries.com/v3.1/all?fields=name,flags")
+        json_response = response.json()
+        length = len(json_response)
+        random_idx = np.random.randint(0, length, 3)
+        selected_capitals = {}
+        for idx in random_idx:
+            country = json_response[idx]
+            name = country["name"]["common"]
+            flag = country["flags"]["svg"]
+            selected_capitals[name] = flag
+        st.session_state.selected_countries = selected_capitals
 
-if st.session_state.get("random_country"):
-    flag = st.session_state.selected_countries[st.session_state.random_country]
-    st.image(flag)
+        random_country = np.random.choice(list(selected_capitals.keys()))
+        st.session_state.random_country = random_country
+        countries = {}
+        for idx, (name, flag) in enumerate(selected_capitals.items()):
+            if name == random_country:
+                countries[name] = True
+            else:
+                countries[name] = False
+        st.session_state.countries = countries
 
+    if st.session_state.get("random_country"):
+        flag = st.session_state.selected_countries[st.session_state.random_country]
+        st.image(flag)
 
-for idx, (name, is_ok) in enumerate(st.session_state.countries.items()):
-    key = f"button_{name}"
-    button = st.button(name, key=key)
-    if button:
-        if is_ok:
-            st.success("Correct!")
-        else:
-            st.error("Incorrect!")
+    for idx, (name, is_ok) in enumerate(st.session_state.countries.items()):
+        key = f"country_button_{name}"
+        button = st.button(name, key=key)
+        if button:
+            if is_ok:
+                st.success("Correct!")
+            else:
+                st.error("Incorrect!")
 
-st.divider()
+    st.divider()
 
-restart = st.button("Play again!")
-if restart:
-    st.session_state.pop("selected_countries")
-    st.session_state.pop("random_country")
-    st.session_state.pop("countries")
-    st.rerun()
+    restart = st.button("Play again!", key="country_play_again")
+    if restart:
+        st.session_state.pop("selected_countries")
+        st.session_state.pop("random_country")
+        st.session_state.pop("countries")
+        st.rerun()
+
+with tab_2:
+    st.subheader("Which country does this capital belong to?")
+
+    if not st.session_state.get("selected_capitals"):
+        response = requests.get(
+            "https://restcountries.com/v3.1/all?fields=name,capital"
+        )
+        json_response = response.json()
+        length = len(json_response)
+        random_idx = np.random.randint(0, length, 3)
+        selected_capitals = {}
+        for idx in random_idx:
+            country = json_response[idx]
+            name = country["name"]["common"]
+            capital = country["capital"][0]
+            selected_capitals[name] = capital
+        st.session_state.selected_capitals = selected_capitals
+
+        random_capital = np.random.choice(list(selected_capitals.keys()))
+        st.session_state.random_capital = random_capital
+        capitals_countries = {}
+        for idx, (name, flag) in enumerate(selected_capitals.items()):
+            if name == random_capital:
+                capitals_countries[name] = True
+            else:
+                capitals_countries[name] = False
+        st.session_state.capitals_countries = capitals_countries
+
+    if st.session_state.get("random_capital"):
+        capital = st.session_state.selected_capitals[st.session_state.random_capital]
+        st.title(capital)
+
+    for idx, (name, is_ok) in enumerate(st.session_state.capitals_countries.items()):
+        key = f"capital_button_{name}"
+        button = st.button(name, key=key)
+        if button:
+            if is_ok:
+                st.success("Correct!")
+            else:
+                st.error("Incorrect!")
+
+    st.divider()
+
+    restart = st.button("Play again!", key="capital_play_again")
+    if restart:
+        st.session_state.pop("selected_countries")
+        st.session_state.pop("random_country")
+        st.session_state.pop("countries")
+        st.rerun()
