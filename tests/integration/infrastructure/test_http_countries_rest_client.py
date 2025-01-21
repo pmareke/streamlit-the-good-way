c8a9d63e696bbd4f15791596de42b366860c6fc1
Pedro López Mareque <pedro.lopez.mareque@gmail.com>
2025-01-21 09:41:18 +0100

    feat: test HttpCountriesRestClient by flag and capital

--- tests/integration/infrastructure/test_http_countries_rest_client.py
+++ tests/integration/infrastructure/test_http_countries_rest_client.py
@@ -1,3 +1,23 @@
+from expects import be_none, contain, expect, have_length
+
+from src.infrastructure.http_countries_rest_client import HttpCountriesRestClientFactory
+
+
 class TestHttpCountriesClientIntegration:
-    def test_find_countries(self) -> None:
-        pass
+    def test_find_countries_by_flag(self) -> None:
+        max = 10
+        client = HttpCountriesRestClientFactory.make()
+
+        flag, booleans = client.find_countries_by_flag(max)
+
+        expect(flag).to(contain("https://flagcdn.com"))
+        expect(booleans).to(have_length(max))
+
+    def test_find_countries_by_capital(self) -> None:
+        max = 10
+        client = HttpCountriesRestClientFactory.make()
+
+        capital, booleans = client.find_countries_by_capital(max)
+
+        expect(capital).not_to(be_none)
+        expect(booleans).to(have_length(max))
