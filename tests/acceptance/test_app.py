c31ba0b1131f47273d1f95c52c5abc7ef385fb15
Pedro López Mareque <pedro.lopez.mareque@gmail.com>
2025-01-22 14:46:45 +0100

    feat: refactor tests

--- tests/acceptance/test_app.py
+++ tests/acceptance/test_app.py
@@ -10,9 +10,12 @@ class TestApp:
 
         header = "Fun with flags and capitals!"
         expect(at.header[0].value).to(equal(header))
+
         subheader = "Which country does this flag belong to?"
         expect(at.subheader[0].value).to(equal(subheader))
+
         expect(at.button).to(have_length(8))  # 4 in each tab
+
         play_again = "Play again!"
         expect(at.button[3].label).to(equal(play_again))
         expect(at.button[7].label).to(equal(play_again))
